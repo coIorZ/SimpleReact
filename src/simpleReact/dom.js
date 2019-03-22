b8f7aeaee0d36fb69f33e46dbe1024cd3e7e15aa
coiorz <coiorz@DESKTOP-8QQLC51.localdomain>
2019-03-22 20:50:59 +0800

    dom diff

--- src/simpleReact/dom.js
+++ src/simpleReact/dom.js
@@ -11,25 +11,25 @@ export function updateNode(node, props, prevProps = {}) {
     if (props.nodeValue !== prevProps.nodeValue)
       node.nodeValue = props.nodeValue;
   } else {
+    Object.keys(prevProps)
+      .filter(isAttr)
+      .forEach(key => {
+        if (!props.hasOwnProperty(key)) {
+          removeAttribute(node, key);
+        }
+      });
+    Object.keys(props)
+      .filter(isAttr)
+      .forEach(key => {
+        if (props[key] !== prevProps[key]) {
+          updateAttribute(node, key, props[key]);
+        }
+      });
     handleListeners(prevProps, (eventName, handler) => {
       node.removeEventListener(eventName, handler);
     });
-    handleAttrs(prevProps, attr => {
-      if (attr === 'value') {
-        node.value = '';
-      } else {
-        node.removeAttribute(attr);
-      }
-    });
     handleListeners(props, (eventName, handler) => {
       node.addEventListener(eventName, handler);
-    });
-    handleAttrs(props, attr => {
-      if (attr === 'value') {
-        node.value = props[attr];
-      } else {
-        node.setAttribute(attr, props[attr]);
-      }
     });
   }
 }
@@ -42,13 +42,26 @@ function handleListeners(props, cb) {
   });
 }
 
-function handleAttrs(props, cb) {
-  const attrs = Object.keys(props).filter(
-    p => !isListener(p) && p !== 'children'
-  );
-  attrs.forEach(cb);
-}
-
 function isListener(str) {
   return str.startsWith('on');
+}
+
+function isAttr(str) {
+  return !isListener(str) && str !== 'children';
+}
+
+function removeAttribute(node, key) {
+  if (key === 'value') {
+    node.value = '';
+  } else {
+    node.removeAttribute(key);
+  }
+}
+
+function updateAttribute(node, key, value) {
+  if (key === 'value') {
+    node.value = value;
+  } else {
+    node.setAttribute(key, value);
+  }
 }
